00a832c0957c83b4fb0b2e10cd1a620a4f023343
SangWon7242 <gotkdqja@gmail.com>
2024-10-26 01:09:29 +0900

    route를 이용하여 화면에 매개변수 전달

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-const HomeScreen = () => {
-  const navigation = useNavigation();
+const HomeScreen = ({ navigation }) => {
+  // const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home Screen</Text>
@@ -13,14 +13,48 @@ const HomeScreen = () => {
         title="Go to Details"
         onPress={() => navigation.navigate("Details")}
       />
+      <Button
+        title="새 할 일 작성"
+        onPress={() => navigation.navigate("TodoWrite")}
+      />
     </View>
   );
 };
 
-const DetailsScreen = ({ navigation }) => {
+const TodoWriteScreen = ({ navigation, route }) => {
+  const [todo, setTodo] = useState("");
+
+  return (
+    <>
+      <TextInput
+        multiline
+        placeholder="할 일을 작성해주세요."
+        style={{ height: 200, padding: 10, backgroundColor: "white" }}
+        value={todo}
+        onChangeText={setTodo}
+      />
+      <Button
+        title="작성"
+        onPress={() => {
+          navigation.navigate({
+            name: "Details",
+            params: { todo },
+            merge: true,
+          });
+        }}
+      />
+    </>
+  );
+};
+
+const DetailsScreen = ({ navigation, route }) => {
+  const todo = route.params;
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Details Screen</Text>
+      <Text>{JSON.stringify(todo)}</Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
       <Button
         title="Go to Details... again"
         onPress={() => navigation.push("Details")}
@@ -36,6 +70,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="TodoWrite" component={TodoWriteScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
