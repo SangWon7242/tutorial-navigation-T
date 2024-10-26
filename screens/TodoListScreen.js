20bad5286ff2fbd586263e3e554d403191737010
SangWon7242 <gotkdqja@gmail.com>
2024-10-27 03:51:04 +0900

    할 일 리스트 레이아웃 조정

--- screens/TodoListScreen.js
+++ screens/TodoListScreen.js
@@ -10,33 +10,34 @@ const TodoListScreen = ({ navigation, route }) => {
     <View style={styles.container}>
       {todos.length > 0 ? (
         todos.map((todo) => (
-          <ListItem.Swipeable
-            key={todo.id}
-            bottomDivider
-            style={styles.listBox}
-            leftContent={(reset) => (
-              <Pressable
-                onPress={() => reset()}
-                style={{ ...styles.pressableBtn, backgroundColor: "blue" }}
-              >
-                <Icon name="update" color="white" />
-              </Pressable>
-            )}
-            rightContent={(reset) => (
-              <Pressable
-                onPress={() => reset()}
-                style={{ ...styles.pressableBtn, backgroundColor: "red" }}
-              >
-                <Icon name="delete" color="white" />
-              </Pressable>
-            )}
-          >
-            <ListItem.Content>
-              <ListItem.Title>번호 : {todo.id}</ListItem.Title>
-              <ListItem.Subtitle>작성날짜 : {todo.regDate}</ListItem.Subtitle>
-              <ListItem.Subtitle>할일 : {todo.content}</ListItem.Subtitle>
-            </ListItem.Content>
-          </ListItem.Swipeable>
+          <View key={todo.id} style={{ marginTop: 5 }}>
+            <ListItem.Swipeable
+              bottomDivider
+              style={styles.listBox}
+              leftContent={(reset) => (
+                <Pressable
+                  onPress={() => reset()}
+                  style={{ ...styles.pressableBtn, backgroundColor: "blue" }}
+                >
+                  <Icon name="update" color="white" />
+                </Pressable>
+              )}
+              rightContent={(reset) => (
+                <Pressable
+                  onPress={() => reset()}
+                  style={{ ...styles.pressableBtn, backgroundColor: "red" }}
+                >
+                  <Icon name="delete" color="white" />
+                </Pressable>
+              )}
+            >
+              <ListItem.Content>
+                <ListItem.Title>번호 : {todo.id}</ListItem.Title>
+                <ListItem.Subtitle>작성날짜 : {todo.regDate}</ListItem.Subtitle>
+                <ListItem.Subtitle>할일 : {todo.content}</ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem.Swipeable>
+          </View>
         ))
       ) : (
         <Text style={styles.emptyText}>할 일이 없습니다</Text>
