fe78f9b24a8b8b5918ec2c23bcdbe2421cbf3bcd
SangWon7242 <gotkdqja@gmail.com>
2024-11-02 02:12:50 +0900

    앱에 배경이미지 반영

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,21 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, ImageBackground } from "react-native";
+
+const backgroundImage = require("../assets/images/todoBgImg.jpg");
 
 const HomeScreen = ({ navigation }) => {
   // const navigation = useNavigation();
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
-    </View>
+    <ImageBackground source={backgroundImage} style={styles.container}>
+      <View style={styles.container}>
+        <Text style={styles.text}>Home Screen</Text>
+      </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
