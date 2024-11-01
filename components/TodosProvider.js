50e094717c2255ce9b241c51269ba72849fce767
SangWon7242 <gotkdqja@gmail.com>
2024-11-02 02:04:42 +0900

    테스트 데이터를 별도의 컴포넌트로 분리

--- components/TodosProvider.js
+++ components/TodosProvider.js
@@ -1,87 +1,10 @@
 import React, { createContext, useState, useEffect, useRef } from "react";
 import { dateToStr } from "../utils/utils";
+import testTodo from "./TodosTestData";
 
 const TodosContext = createContext();
 
 export const TodosProvider = ({ children }) => {
-  const testTodo = [
-    {
-      id: 1,
-      regDate: dateToStr(new Date()),
-      content: "테니스 치기",
-    },
-    {
-      id: 2,
-      regDate: dateToStr(new Date()),
-      content: "리액트 네이티브 공부하기",
-    },
-    {
-      id: 3,
-      regDate: dateToStr(new Date()),
-      content: "할 일 앱 구현하기",
-    },
-    {
-      id: 4,
-      regDate: dateToStr(new Date()),
-      content: "테니스 치기",
-    },
-    {
-      id: 5,
-      regDate: dateToStr(new Date()),
-      content: "리액트 네이티브 공부하기",
-    },
-    {
-      id: 6,
-      regDate: dateToStr(new Date()),
-      content: "할 일 앱 구현하기",
-    },
-    {
-      id: 7,
-      regDate: dateToStr(new Date()),
-      content: "테니스 치기",
-    },
-    {
-      id: 8,
-      regDate: dateToStr(new Date()),
-      content: "리액트 네이티브 공부하기",
-    },
-    {
-      id: 9,
-      regDate: dateToStr(new Date()),
-      content: "할 일 앱 구현하기",
-    },
-    {
-      id: 10,
-      regDate: dateToStr(new Date()),
-      content: "테니스 치기",
-    },
-    {
-      id: 11,
-      regDate: dateToStr(new Date()),
-      content: "리액트 네이티브 공부하기",
-    },
-    {
-      id: 12,
-      regDate: dateToStr(new Date()),
-      content: "할 일 앱 구현하기",
-    },
-    {
-      id: 13,
-      regDate: dateToStr(new Date()),
-      content: "테니스 치기",
-    },
-    {
-      id: 14,
-      regDate: dateToStr(new Date()),
-      content: "리액트 네이티브 공부하기",
-    },
-    {
-      id: 15,
-      regDate: dateToStr(new Date()),
-      content: "할 일 앱 구현하기",
-    },
-  ];
-
   // const [todos, setTodos] = useState([]);
   // const lastTodoIdRef = useRef(0);
 
---
+++ components/TodosTestData.js
@@ -0,0 +1,81 @@
+import { dateToStr } from "../utils/utils";
+
+const testTodo = [
+  {
+    id: 1,
+    regDate: dateToStr(new Date()),
+    content: "테니스 치기",
+  },
+  {
+    id: 2,
+    regDate: dateToStr(new Date()),
+    content: "리액트 네이티브 공부하기",
+  },
+  {
+    id: 3,
+    regDate: dateToStr(new Date()),
+    content: "할 일 앱 구현하기",
+  },
+  {
+    id: 4,
+    regDate: dateToStr(new Date()),
+    content: "테니스 치기",
+  },
+  {
+    id: 5,
+    regDate: dateToStr(new Date()),
+    content: "리액트 네이티브 공부하기",
+  },
+  {
+    id: 6,
+    regDate: dateToStr(new Date()),
+    content: "할 일 앱 구현하기",
+  },
+  {
+    id: 7,
+    regDate: dateToStr(new Date()),
+    content: "테니스 치기",
+  },
+  {
+    id: 8,
+    regDate: dateToStr(new Date()),
+    content: "리액트 네이티브 공부하기",
+  },
+  {
+    id: 9,
+    regDate: dateToStr(new Date()),
+    content: "할 일 앱 구현하기",
+  },
+  {
+    id: 10,
+    regDate: dateToStr(new Date()),
+    content: "테니스 치기",
+  },
+  {
+    id: 11,
+    regDate: dateToStr(new Date()),
+    content: "리액트 네이티브 공부하기",
+  },
+  {
+    id: 12,
+    regDate: dateToStr(new Date()),
+    content: "할 일 앱 구현하기",
+  },
+  {
+    id: 13,
+    regDate: dateToStr(new Date()),
+    content: "테니스 치기",
+  },
+  {
+    id: 14,
+    regDate: dateToStr(new Date()),
+    content: "리액트 네이티브 공부하기",
+  },
+  {
+    id: 15,
+    regDate: dateToStr(new Date()),
+    content: "할 일 앱 구현하기",
+  },
+];
+
+export default testTodo;
